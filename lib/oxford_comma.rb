f867dbe130294ffc3fbbf95b5fa5840c2886e3db
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 22:31:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case oxford
+  when array.length == 1
+    return array.join
+  when array.length == 2
+    return array.join ' and '
+  when array.length >= 3
+    last = array.pop
+    formatted = array.join ', ' + ", and #{last}"
+  end
+    
 end
